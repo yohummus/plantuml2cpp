890ee8e12d1913986bc263bc53de1a9688ff6855
Johannes Bergmann <j-bergmann@outlook.com>
2020-09-12 14:41:20 +0200

    Set version number to 0.0.2

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.md', 'r') as f:
 
 setuptools.setup(
     name='plantuml2cpp',
-    version='0.0.1',
+    version='0.0.2',
     author='Johannes Bergmann',
     author_email='j-bergmann@outlook.com',
     description='C++ code generator for finite state machines from PlantUML state diagrams',
